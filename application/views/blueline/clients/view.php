dba2ecc2f2d00a8199ed44e4af04f33654054d73
julio.cesar <julio.cesar@locaweb.com.br>
2019-09-01 22:03:05 -0300

    Fix show client view by id
    - Change companie by client

--- application/views/blueline/clients/view.php
+++ application/views/blueline/clients/view.php
@@ -1,41 +1,39 @@
 <div class="row">
    <div class="col-md-12">
-      <h2><?=$company->name;?></h2>
+      <h2><?=$client->firstname;?></h2>
    </div>
 </div>
 <div class="row">
    <div class="col-md-12 marginbottom20">
       <div class="table-head">
-         <?=$this->lang->line('application_client_details');?> <span class="pull-right"><a href="<?=base_url()?>clients/company/update/<?=$company->id;?>/view" class="btn btn-primary" data-toggle="mainmodal"><i class="icon-edit"></i> <?=$this->lang->line('application_edit');?></a></div>
+         <?=$this->lang->line('application_client_details');?> <span class="pull-right"><a href="<?=base_url()?>clients/company/update/<?=$client->id;?>/view" class="btn btn-primary" data-toggle="mainmodal"><i class="icon-edit"></i> <?=$this->lang->line('application_edit');?></a></div>
 		<div class="subcont">
 		<ul class="details col-md-6">
 			<li><span><?=$this->lang->line('application_company_name');?>:</span>
-            <?php echo $company->name = empty($company->name) ? "-" : $company->name; ?></li>
+            <?php echo $client->firstname = empty($client->firstname) ? "-" : $client->firstname; ?></li>
             <li><span><?=$this->lang->line('application_primary_contact');?>:</span>
-               <?php if(isset($company->client->firstname)){ echo $company->client->firstname.' '.$company->client->lastname;}else{echo "-";} ?></li>
+               <?php if(isset($client->firstname)){ echo $client->firstname.' '.$client->lastname;}else{echo "-";} ?></li>
             <li><span><?=$this->lang->line('application_email');?>:</span>
-               <?php if(isset($company->client->email)){ echo $company->client->email; }else{ echo "-"; } ?></li>
-            <li><span><?=$this->lang->line('application_website');?>:</span>
-               <?php echo $company->website = empty($company->website) ? "-" : '<a target="_blank" href="http://'.$company->website.'">'.$company->website.'</a>' ?></li>
+               <?php if(isset($client->email)){ echo $client->email; }else{ echo "-"; } ?></li>
             <li><span><?=$this->lang->line('application_phone');?>:</span>
-               <?php echo $company->phone = empty($company->phone) ? "-" : $company->phone; ?></li>
-            <?php if($company->vat != ""){?>
+               <?php echo $client->phone = empty($client->phone) ? "-" : $client->phone; ?></li>
+            <?php if($client->company->vat != ""){?>
             <li><span><?=$this->lang->line('application_vat');?>:</span>
-               <?php echo $company->vat; ?></li>
+               <?php echo $client->company->vat; ?></li>
             <?php } ?>
             </ul>
             <span class="visible-xs"></span>
             <ul class="details col-md-6">
                <li><span><?=$this->lang->line('application_address');?>:</span>
-                  <?php echo $company->address = empty($company->address) ? "-" : $company->address; ?></li>
+                  <?php echo $client->address = empty($client->address) ? "-" : $client->address; ?></li>
                <li><span><?=$this->lang->line('application_zip_code');?>:</span>
-                  <?php echo $company->zipcode = empty($company->zipcode) ? "-" : $company->zipcode; ?></li>
+                  <?php echo $client->zipcode = empty($client->zipcode) ? "-" : $client->zipcode; ?></li>
                <li><span><?=$this->lang->line('application_city');?>:</span>
-                  <?php echo $company->city = empty($company->city) ? "-" : $company->city; ?></li>
+                  <?php echo $client->city = empty($client->city) ? "-" : $client->city; ?></li>
                <li><span><?=$this->lang->line('application_country');?>:</span>
-                  <?php echo $company->country = empty($company->country) ? "-" : $company->country; ?></li>
+                  <?php echo $client->company->country = empty($client->company->country) ? "-" : $client->company->country; ?></li>
                <li><span><?=$this->lang->line('application_province');?>:</span>
-                  <?php echo $company->province = empty($company->province) ? "-" : $company->province; ?></li>
+                  <?php echo $client->company->province = empty($client->company->province) ? "-" : $client->company->province; ?></li>
 
             </ul>
             <br clear="all">
